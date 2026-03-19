88f7448626d884e1fb849af9b24569ab113fcaa8
kakeinbruh-rgb <kakeinbruh@gmail.com>
2026-03-19 22:33:45 +0500

    Time: 8 ms (35.54%) | Memory: 22.3 MB (91.50%) - LeetSync

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,25 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+        # If min_stack is empty or val is smaller, push it
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+        else:
+            # Repeat the current minimum
+            self.min_stack.append(self.min_stack[-1])
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
